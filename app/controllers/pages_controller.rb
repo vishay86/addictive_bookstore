b687c5ddac6a67b3fc2b9b0991d72a22727a2075
Vishay Bhatia <vishay@addictivegifts.com>
2015-06-26 18:59:07 +0400

    Created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only:[:dashboard] 
   def home
   	if current_user
   		redirect_to books_path
@@ -7,5 +8,9 @@ class PagesController < ApplicationController
 
 
   def about
+  end
+
+  def dashboard
+  	@books = current_user.books
   end
 end
